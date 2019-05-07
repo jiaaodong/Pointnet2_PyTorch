f608df91cbe33cbd5cb52e3b00df1f0318228dc6
Jiaao Dong <j.dong-1@student.tudelft.nl>
2019-05-07 10:23:35 +0200

    remove unnecessary argument for testing function

--- pointnet2/data/Indoor3DSemSegLoader.py
+++ pointnet2/data/Indoor3DSemSegLoader.py
@@ -105,7 +105,7 @@ class Indoor3DSemSeg(data.Dataset):
 
 
 if __name__ == "__main__":
-    dset = Indoor3DSemSeg(16, "./", train=True)
+    dset = Indoor3DSemSeg(16, train=True)
     print(dset[0])
     print(len(dset))
     dloader = torch.utils.data.DataLoader(dset, batch_size=32, shuffle=True)
