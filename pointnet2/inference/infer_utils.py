6c5c449a2fda954aecefb50e404d1724162366b2
Jiaao Dong <j.dong-1@student.tudelft.nl>
2019-05-23 15:00:33 +0200

    add inference part

---
+++ pointnet2/inference/infer_utils.py
@@ -0,0 +1,82 @@
+
+import sys 
+import os.path as osp 
+import os 
+import argparse
+import numpy as np 
+
+
+from pointnet2.models import Pointnet2SemMSG as Pointnet
+import torch 
+import torch.nn 
+import h5py
+import matplotlib.pyplot as plt 
+import time
+
+from torch.utils.data import DataLoader
+import etw_pytorch_utils as pt_utils
+from pointnet2.data import RadarLowLvlSemSeg
+
+import tqdm
+
+from copy import deepcopy
+
+
+
+
+
+class Tester(object):
+    def __init__(self, model, loss_func, use_gpu):
+        self.model = model
+        self.loss_func = loss_func
+        self.use_gpu = use_gpu
+        if use_gpu:
+            self.model.cuda()
+        self.model.eval()
+
+    def test(self, dataloader):
+        total_loss = 0
+        cnter = 1
+        pred_labels_all = np.array([])
+        true_labels_all = np.array([])
+        with tqdm.tqdm(total = len(dataloader), leave=False, desc='test') as pbar:
+            for i, batch in enumerate(dataloader):
+                pbar.update()
+                pred_labels, labels, res_dict = self._test_it(batch)
+                total_loss += res_dict['loss']
+                cnter+=1
+                pred_labels_np, labels_np = pred_labels.cpu().numpy(), labels.cpu().numpy()
+                # print(pred_labels_np.shape)
+                if i == 0:
+                    pred_labels_all = pred_labels_np
+                    true_labels_all = labels_np
+                else:
+                    pred_labels_all = np.concatenate((pred_labels_all, pred_labels_np), axis=0)
+                    true_labels_all = np.concatenate((true_labels_all, labels_np), axis=0)
+            pred_labels_all = pred_labels_all
+            true_labels_all = true_labels_all
+            loss = total_loss/cnter
+
+        return loss, pred_labels_all, true_labels_all
+
+
+    def _test_it(self, batch):
+        self.model.zero_grad()
+        classes, loss, res_dict = self._forward_pass(batch)
+        return classes, loss, res_dict 
+
+    def _forward_pass(self, batch):
+        inputs, labels = batch
+        if self.use_gpu:
+            inputs = inputs.cuda()
+            labels = labels.cuda()
+        self.model.eval()
+        preds = self.model(inputs)
+        loss = self.loss_func(preds.view(labels.numel(),-1), labels.view(-1))
+        _, pred_labels = torch.max(preds, -1)
+        acc = (pred_labels.view(-1) == labels.view(-1)).float().sum() / labels.numel()
+        res_dict = {
+            "loss": loss.item(),
+            "acc": acc.item()
+        }
+        return pred_labels, labels, res_dict
--- pointnet2/inference/inference.py
+++ pointnet2/inference/inference.py
@@ -7,29 +7,34 @@ import numpy as np
 
 from pointnet2.models import Pointnet2SemMSG as Pointnet
 import torch 
-import torch.nn 
+import torch.nn as nn 
 import h5py
 import matplotlib.pyplot as plt 
 import time
+from sklearn.metrics import confusion_matrix
+from plot_confusion_matrix import plot_confusion_matrix
+
+from torch.utils.data import DataLoader
+import etw_pytorch_utils as pt_utils
+from pointnet2.data import RadarLowLvlSemSeg
+
+import tqdm
 
 from copy import deepcopy
+from infer_utils import Tester
 
 home = osp.expanduser("~")
 top_folder = osp.join(osp.dirname(os.path.abspath(__file__)), "..")
 model_path = osp.join(top_folder, 'train','checkpoints','poitnet2_semseg_best.pth.tar')
-data_path = osp.join(top_folder, "data","RadarLowLevel","radar_pcd_dataset_multi_baseline.h5")
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-model", type = str, default = model_path)
-parser.add_argument("-data", type= str, default = data_path)
-parser.add_argument("-root", type= str, default = osp.join("/data", 'jiaaodong','conti_new','dataset_daimler'))
+parser.add_argument("-root", type= str, default = osp.join("/data", 'jiaaodong','conti_new','dataset1'))
+parser.add_argument("-data", type= str, default = "/home/jiaaodong/Pointnet2_PyTorch/pointnet2/data/RadarLowLevel/LLTpointnet2radar_pcd_dataset_multi.h5")
 args = parser.parse_args()
 
 def load_pointnet(model_path):
-    model = Pointnet(num_classes=4, input_channels=2, use_xyz=True)
-    model.eval()
-    model.cuda()
-
+    model = Pointnet(num_classes=4, input_channels=72, use_xyz=False)
     if os.path.isfile(model_path):
         print("==> Loading from checkpoint '{}'".format(model_path))
         checkpoint = torch.load(model_path)
@@ -45,46 +50,111 @@ def load_data(data_path):
     data_file  = h5py.File(data_path)
     features = data_file["data"][:]
     labels = data_file["label"][:]
+    features = np.insert(features, 2, 0, axis=2)
     return features, labels 
 
 
+
+
 if __name__ == "__main__":
-    
-    data_path = args.data
+    use_gpu = True
     model_path = args.model
     root_path = args.root
+    data_path = args.data
+    num_pts = 512
+    draw_plots = True
+
+    train_set = RadarLowLvlSemSeg(num_pts, train_rat=0.6)
+    train_loader = DataLoader(
+        train_set,
+        batch_size=1024,
+        pin_memory=True,
+        num_workers=2,
+        shuffle=False,
+    )
+
+    test_set = RadarLowLvlSemSeg(num_pts, train=False)
+    test_loader = DataLoader(
+        test_set,
+        batch_size=1024,
+        shuffle=False,
+        pin_memory=True,
+        num_workers=2,
+    )
+
+    model = load_pointnet(model_path)
+    weights = [0.00312226, 0.51293818, 0.42739984, 0.05653972]
+    weights = torch.tensor(weights)
+    if use_gpu:
+        weights = weights.cuda().float()
+    loss_func = nn.CrossEntropyLoss(weights)
+    tester = Tester(
+        model,
+        loss_func = loss_func,
+        use_gpu=use_gpu
+    )
+
+    loss_train, pred_labels_train, true_labels_train = tester.test(
+        train_loader
+    )
+
+    loss_val, pred_labels_val, true_labels_val = tester.test(
+        test_loader
+    )
+
+    np.save(osp.join(root_path, "pred_labels_train_pnt2"), pred_labels_train)
+    np.save(osp.join(root_path, "true_labels_train_pnt2"), true_labels_train)
+    np.save(osp.join(root_path, "pred_labels_val_pnt2"), pred_labels_val)
+    np.save(osp.join(root_path, "true_labels_val_pnt2"), true_labels_val)
+
+    class_names = np.array(['background', 'pedestrian', 'rider', 'car'])
+    confusion_mat = confusion_matrix(true_labels_val.flatten(), pred_labels_val.flatten())
+    fig, ax1=plt.subplots(figsize=(8, 7))
+    plot_confusion_matrix(confusion_mat, classes=class_names,
+                title='pnt2_without_normalization', ax = ax1)
+    plt.savefig(osp.join(root_path, 'pnt2_unnormalized'))
+
+    fig, ax1=plt.subplots(figsize=(8, 7))
+    plot_confusion_matrix(confusion_mat, classes=class_names,normalize=True,
+                title='pnt2_normalization', ax = ax1)
+    plt.savefig(osp.join(root_path,  'pnt2_normalized'))
+    np.savetxt(osp.join(root_path, 'pnt2.txt'),confusion_mat,fmt='%.2f')
+    np.save(osp.join('pnt2_conmat.npy'),confusion_mat)
+
+
+
+    features, _ = load_data(data_path)
+    features = features[:, :, :3]
     img_path = osp.join(root_path, "pointnet_result")
+    color_map = ['y','g','r','b']
     if not osp.exists(img_path):
         print("Creating image folder...")
         os.makedirs(img_path)
-    features, labels = load_data(data_path)
-    points_all = deepcopy(features[:, :, :3])
-    model = load_pointnet(model_path)
-    #print("shape of the features:{} and labels:{} and pred:{}, inference time:{}".format(features.shape, labels.shape, pred.shape,inference_time))
+    # pred_labels_train = np.reshape(pred_labels_train, (-1, num_pts))
+    # true_labels_train = np.reshape(true_labels_train, (-1, num_pts))
+    # pred_labels_val = np.reshape(pred_labels_val, (-1, num_pts))
+    # true_labels_val = np.reshape(pred_labels_val, (-1, num_pts))
+    pred_labels = np.concatenate((pred_labels_train, pred_labels_val), axis=0)
+    true_labels = np.concatenate((true_labels_train, true_labels_val), axis=0)
 
-    for i, points_single in enumerate(points_all):
-        features_single = torch.tensor(np.expand_dims(features[i,...],axis=0)).type(torch.FloatTensor)
-        t1 = time.time()
-        pred = model(features_single.to("cuda", non_blocking=True))
-        _, pred = torch.max(pred, -1)
-        pred = pred.cpu().detach().numpy()
-        t2 = time.time()
-        inference_time = t2-t1 
-        fig = plt.figure(figsize=(16,8))
-        ax1 = plt.subplot(121)
-        sc1 = ax1.scatter(points_single[:,1], points_single[:,0], c=labels[i, :])
-        ax1.set_ylim([0,100])
-        ax1.set_xlim([-30,30])
-        ax1.set_title("Input")
-        plt.colorbar(sc1)
-        ax2 = plt.subplot(122)
-        sc2=ax2.scatter(points_single[:,1], points_single[:,0], c=pred[0,:])
-        ax2.set_ylim([0,100])
-        ax2.set_xlim([-30,30])
-        ax2.set_title("Prediction")
-        plt.colorbar(sc2)
-        plt.savefig(osp.join(img_path,"{:05d}.jpg".format(i)))
-        plt.close()
-        print("finished {}th frame in {} inference time".format(i, inference_time))
-        # plt.show()
-
+    if draw_plots:
+        pbar = tqdm.tqdm(total = features.shape[0])
+        for i in range(features.shape[0]):
+            pbar.update()
+            fig = plt.figure(figsize=(16,8))
+            ax1 = plt.subplot(121)
+            sc1 = ax1.scatter(features[i,:,1], features[i,:,0], c=[color_map[int(cindx)] for cindx in true_labels[i, :]])
+            ax1.set_ylim([0,100])
+            ax1.set_xlim([-30,30])
+            ax1.set_title("Input")
+            # plt.colorbar(sc1)
+            ax2 = plt.subplot(122)
+            sc2=ax2.scatter(features[i,:,1], features[i, :,0], c= [color_map[int(cindx)] for cindx in pred_labels[0, :]])
+            ax2.set_ylim([0,100])
+            ax2.set_xlim([-30,30])
+            ax2.set_title("Prediction")
+            # plt.colorbar(sc2)
+            plt.savefig(osp.join(img_path,"{:05d}.jpg".format(i)))
+            plt.close()
+            # plt.show()
+    pbar.close()
