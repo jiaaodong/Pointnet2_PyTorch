8ea2ce60d06fdf4f096a3a11c73db86ba1213137
Jiaao Dong <j.dong-1@student.tudelft.nl>
2019-05-24 09:32:05 +0200

    add f1score

--- pointnet2/inference/inference.py
+++ pointnet2/inference/inference.py
@@ -12,6 +12,7 @@ import h5py
 import matplotlib.pyplot as plt 
 import time
 from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
 from plot_confusion_matrix import plot_confusion_matrix
 
 from torch.utils.data import DataLoader
@@ -30,7 +31,7 @@ model_path = osp.join(top_folder, 'train','checkpoints','poitnet2_semseg_best.pt
 parser = argparse.ArgumentParser()
 parser.add_argument("-model", type = str, default = model_path)
 parser.add_argument("-root", type= str, default = osp.join("/data", 'jiaaodong','conti_new','dataset1'))
-parser.add_argument("-data", type= str, default = "/home/jiaaodong/Pointnet2_PyTorch/pointnet2/data/RadarLowLevel/LLTpointnet2radar_pcd_dataset_multi.h5")
+parser.add_argument("-data", type= str, default = "/home/jiaaodong/Pointnet2_PyTorch/pointnet2/data/RadarLowLevel/LLT_without_static.h5")
 args = parser.parse_args()
 
 def load_pointnet(model_path):
@@ -50,7 +51,7 @@ def load_data(data_path):
     data_file  = h5py.File(data_path)
     features = data_file["data"][:]
     labels = data_file["label"][:]
-    features = np.insert(features, 2, 0, axis=2)
+    # features = np.insert(features, 2, 0, axis=2)
     return features, labels 
 
 
@@ -61,8 +62,12 @@ if __name__ == "__main__":
     model_path = args.model
     root_path = args.root
     data_path = args.data
-    num_pts = 512
+    num_pts = 128
     draw_plots = True
+    result_folder = "rm_static_nearby"
+    result_folder = osp.join(root_path, result_folder)
+    if not osp.exists(result_folder):
+        os.makedirs(result_folder)
 
     train_set = RadarLowLvlSemSeg(num_pts, train_rat=0.6)
     train_loader = DataLoader(
@@ -97,64 +102,46 @@ if __name__ == "__main__":
     loss_train, pred_labels_train, true_labels_train = tester.test(
         train_loader
     )
+    print("loss train:{}".format(loss_train))
 
     loss_val, pred_labels_val, true_labels_val = tester.test(
         test_loader
     )
+    print("loss val:{}".format(loss_val))
 
-    np.save(osp.join(root_path, "pred_labels_train_pnt2"), pred_labels_train)
-    np.save(osp.join(root_path, "true_labels_train_pnt2"), true_labels_train)
-    np.save(osp.join(root_path, "pred_labels_val_pnt2"), pred_labels_val)
-    np.save(osp.join(root_path, "true_labels_val_pnt2"), true_labels_val)
+    np.save(osp.join(result_folder, "pred_labels_train_pnt2"), pred_labels_train)
+    np.save(osp.join(result_folder, "true_labels_train_pnt2"), true_labels_train)
+    np.save(osp.join(result_folder, "pred_labels_val_pnt2"), pred_labels_val)
+    np.save(osp.join(result_folder, "true_labels_val_pnt2"), true_labels_val)
 
     class_names = np.array(['background', 'pedestrian', 'rider', 'car'])
     confusion_mat = confusion_matrix(true_labels_val.flatten(), pred_labels_val.flatten())
+    f1_score_val = f1_score(true_labels_val, pred_labels_val)
     fig, ax1=plt.subplots(figsize=(8, 7))
     plot_confusion_matrix(confusion_mat, classes=class_names,
-                title='pnt2_without_normalization', ax = ax1)
-    plt.savefig(osp.join(root_path, 'pnt2_unnormalized'))
+                title='pnt2_without_normalization_val', ax = ax1)
+    plt.savefig(osp.join(result_folder, 'pnt2_unnormalized_val'))
 
     fig, ax1=plt.subplots(figsize=(8, 7))
     plot_confusion_matrix(confusion_mat, classes=class_names,normalize=True,
-                title='pnt2_normalization', ax = ax1)
-    plt.savefig(osp.join(root_path,  'pnt2_normalized'))
-    np.savetxt(osp.join(root_path, 'pnt2.txt'),confusion_mat,fmt='%.2f')
-    np.save(osp.join('pnt2_conmat.npy'),confusion_mat)
+                title='pnt2_normalization_val', ax = ax1)
+    plt.savefig(osp.join(result_folder,  'pnt2_normalized_val'))
+    np.savetxt(osp.join(result_folder, 'pnt2_val.txt'),confusion_mat,fmt='%.2f')
+    np.savetxt(osp.join(result_folder, 'f1score_val.txt'),f1_score_val,fmt='%.2f')
+    np.save(osp.join(result_folder, 'pnt2_conmat_val.npy'),confusion_mat)
 
+    confusion_mat = confusion_matrix(true_labels_train.flatten(), pred_labels_train.flatten())
+    f1_score_val = f1_score(true_labels_train, pred_labels_train)
+    fig, ax1=plt.subplots(figsize=(8, 7))
+    plot_confusion_matrix(confusion_mat, classes=class_names,
+                title='pnt2_without_normalization_train', ax = ax1)
+    plt.savefig(osp.join(result_folder, 'pnt2_unnormalized_train'))
 
+    fig, ax1=plt.subplots(figsize=(8, 7))
+    plot_confusion_matrix(confusion_mat, classes=class_names,normalize=True,
+                title='pnt2_normalization_train', ax = ax1)
+    plt.savefig(osp.join(result_folder,  'pnt2_normalized_train'))
+    np.savetxt(osp.join(result_folder, 'pnt2_train.txt'),confusion_mat,fmt='%.2f')
+    np.savetxt(osp.join(result_folder, 'f1score_train.txt'),f1_score_val,fmt='%.2f')
+    np.save(osp.join(result_folder, 'pnt2_conmat_train.npy'),confusion_mat)
 
-    features, _ = load_data(data_path)
-    features = features[:, :, :3]
-    img_path = osp.join(root_path, "pointnet_result")
-    color_map = ['y','g','r','b']
-    if not osp.exists(img_path):
-        print("Creating image folder...")
-        os.makedirs(img_path)
-    # pred_labels_train = np.reshape(pred_labels_train, (-1, num_pts))
-    # true_labels_train = np.reshape(true_labels_train, (-1, num_pts))
-    # pred_labels_val = np.reshape(pred_labels_val, (-1, num_pts))
-    # true_labels_val = np.reshape(pred_labels_val, (-1, num_pts))
-    pred_labels = np.concatenate((pred_labels_train, pred_labels_val), axis=0)
-    true_labels = np.concatenate((true_labels_train, true_labels_val), axis=0)
-
-    if draw_plots:
-        pbar = tqdm.tqdm(total = features.shape[0])
-        for i in range(features.shape[0]):
-            pbar.update()
-            fig = plt.figure(figsize=(16,8))
-            ax1 = plt.subplot(121)
-            sc1 = ax1.scatter(features[i,:,1], features[i,:,0], c=[color_map[int(cindx)] for cindx in true_labels[i, :]])
-            ax1.set_ylim([0,100])
-            ax1.set_xlim([-30,30])
-            ax1.set_title("Input")
-            # plt.colorbar(sc1)
-            ax2 = plt.subplot(122)
-            sc2=ax2.scatter(features[i,:,1], features[i, :,0], c= [color_map[int(cindx)] for cindx in pred_labels[0, :]])
-            ax2.set_ylim([0,100])
-            ax2.set_xlim([-30,30])
-            ax2.set_title("Prediction")
-            # plt.colorbar(sc2)
-            plt.savefig(osp.join(img_path,"{:05d}.jpg".format(i)))
-            plt.close()
-            # plt.show()
-    pbar.close()
